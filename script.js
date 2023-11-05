605d0a8b8f8d8fbac7b102925a5099807b04467c
Tyler Downing <tyler.ar.downing@hotmail.com>
2023-11-05 13:25:09 -0400

    Added UI to RPS game using JS

--- script.js
+++ script.js
@@ -7,41 +7,74 @@ function getComputerChoice() {
 
 // This function actually plays each round, players input, vs the computers. 
 function playRound(play1, play2) {
-    play1 = play1.toLowerCase();
      if (play1 == 'rock' && play2 == 'scisors'){
-         result = true;
+         result = 'win';
      }
      else if (play1 == 'scisors' && play2 == 'paper'){
-     result = true;
+     result = 'win';
      }
      else if (play1 == 'paper' && play2 == 'rock'){
-         result = true;
+         result = 'win';
      }
-     else if( play1 == play2) {
-         return console.log('Its a tie, play again');
-     }
-     else {
-         result = false;
-     }
- 
-     if(result == true) {
-         console.log('You win, ' + play1 + ' beats ' + play2 + "!");
+     else if (play1 == play2) {
+         result = 'tie';
      }
      else {
-         console.log('You lose, ' + play2 + ' beats ' + play1 + "!");
+        result = 'lose';
      }
-     
+     if (result == 'win') {
+        yourScore++;
+        const resultMessage = document.querySelector('#result');
+
+        const message = document.createElement('p');
+        message.classList.add('message');
+        message.textContent = 'You win, ' + play1 + ' beats ' + play2 + "!";
+        resultMessage.appendChild(message);
     }
-    
-// Game play function, input the number of games you want to play.
-function game(numOfGames) {
-    let result;
-    let i = 1;
-    while (i <= numOfGames) {
-        const playerSelection = prompt('Whats your play?');
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        i++;
+    else if (result == 'lose') {
+        computerScore++;
+        const resultMessage = document.querySelector('#result');
+
+        const message = document.createElement('p');
+        message.classList.add('message');
+        message.textContent = 'You lose, ' + play2 + ' beats ' + play1 + "!";
+        resultMessage.appendChild(message);
     }
-}
-console.log(game(5));
+    else {
+        const resultMessage = document.querySelector('#result');
+
+        const message = document.createElement('p');
+        message.classList.add('message');
+        message.textContent = 'Its a tie, play again.';
+        resultMessage.appendChild(message);
+    }
+    const scoreMessage = document.querySelector('#result');
+
+    const score = document.createElement('p');
+    score.classList.add('score');
+    score.textContent = 'Your Score: ' + yourScore + ' Computer Score: ' + computerScore ;
+
+    scoreMessage.appendChild(score);
+
+    }
+    let yourScore = 0;
+    let computerScore = 0;
+// Event listener that plays when you click Rock button
+const playRock = document.querySelector('#rock');
+playRock.addEventListener('click', () => {
+    playRound("rock", getComputerChoice());
+})
+
+// Event listener that plays when you click paper button
+const playPaper = document.querySelector('#paper');
+playPaper.addEventListener('click', () => {
+    playRound("paper", getComputerChoice());
+        
+})
+
+// Event listener that plays when you click scisors button
+const playScisors = document.querySelector('#scisors');
+playScisors.addEventListener('click', () => {
+    playRound("scisors", getComputerChoice());
+        
+})
